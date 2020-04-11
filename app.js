f2e9d65bce4ed5bc552230005a8a94a5590e3f60
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-11 18:25:49 +0300

    Refactor routes via Module exports

--- app.js
+++ app.js
@@ -1,58 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const { url, method } = req;
+const routes = require('./routes');
 
-    if (url === '/') {
-        res.write(`
-        <html>
-            <head>
-                <title>Enter message</title>
-            </head>
-            <body>
-                <form action="/message" method="POST">
-                    <input type="text" name="message"/>
-                    <button>Submit</button>
-                </form>
-            </body>
-        </html>
-        `);
-
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write(`
-        <html>
-            <head>
-                <title>My First Page</title>
-            </head>
-            <body>
-                <h1>Hello from my Node.js Server!</h1>
-            </body>
-        </html>
-    `);
-
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
