66c4c41f3354fe79895039f36f3bf62c9a5f37d0
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-11 18:38:41 +0300

    Assignment

--- routes.js
+++ routes.js
@@ -7,12 +7,20 @@ const requestHandler = (req, res) => {
         res.write(`
         <html>
             <head>
-                <title>Enter message</title>
+                <title>Assignment</title>
             </head>
             <body>
-                <form action="/message" method="POST">
-                    <input type="text" name="message"/>
-                    <button>Submit</button>
+                <h1>Some Greeting Text!</h1>
+                <ul>
+                    <li>User 1</li>
+                    <li>User 2</li>                
+                    <li>User 3</li>                
+                    <li>User 4</li>                
+                    <li>User 5</li>                                
+                </ul>
+                <form action="/create-user" method="POST">
+                    <input name="username" type="text"/>
+                    <button>Submit</button>                    
                 </form>
             </body>
         </html>
@@ -21,7 +29,7 @@ const requestHandler = (req, res) => {
         return res.end();
     }
 
-    if (url === '/message' && method === 'POST') {
+    if (url === '/create-user' && method === 'POST') {
         const body = [];
 
         req.on('data', (chunk) => {
@@ -30,28 +38,16 @@ const requestHandler = (req, res) => {
 
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
+            const username = parsedBody.split('=')[1];
+            fs.writeFile('username.txt', username, (err) => {
+                console.log(username);
+
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
                 return res.end();
             });
         });
     }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write(`
-        <html>
-            <head>
-                <title>My First Page</title>
-            </head>
-            <body>
-                <h1>Hello from my Node.js Server!</h1>
-            </body>
-        </html>
-    `);
-
-    res.end();
 };
 
 module.exports = requestHandler;
