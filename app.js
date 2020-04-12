d1afbbec7fbf1bfd8ec96d95dc4eb8e8109108e3
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-12 21:46:54 +0300

    Adding 404 Error page

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoute);
 app.use(shopRoute);
 
+app.use((req, res, next) => {
+    res
+        .status(404)
+        .send(`
+            <h1>404</h1>
+        `);
+});
+
 app.listen(3000);
