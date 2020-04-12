c9fe20786d050819254d6c2d7680bc6909e331d9
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-12 20:56:12 +0300

    Add new middleware, limit middleware for POST requests

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('Always');
-    next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
     res.send(`
         <h1>Add Product</h1>
+        <form action="/product" method="POST">
+            <input type="text" name="title"/>
+            <button type="submit">Add Product</button>
+        </form>
     `);
 });
 
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
     res.send(`
         <h1>Hello</h1>
     `);
