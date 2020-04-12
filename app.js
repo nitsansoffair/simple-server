a84d712aa57cdda4c8e18ecd99467a1839ad80d7
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-04-12 20:33:46 +0300

    Express.js

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use('/', (req, res, next) => {
+    console.log('Always');
+    next();
+});
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send(`
+        <h1>Add Product</h1>
+    `);
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send(`
+        <h1>Hello</h1>
+    `);
+});
+
+app.listen(3000);
